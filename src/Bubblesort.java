a0c3228a25374a2a3c1c26535a43f0d75abb3343
pc5740 <pc5740@CSELAB03-14.bennett.edu.in>
2019-01-29 11:34:03 +0530

    bubble sort

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -31,6 +31,7 @@ class Bubblesort
         int arr[] = {64, 34, 25, 12, 22, 11, 90};
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
+        System.out.println("Sorted array");
         ob.printArray(arr);
     }
 }
